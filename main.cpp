9b3fa18a12524343ab21336a92996e48a6642970
supermaximo93 <supermaximo93@gmail.com>
2012-01-19 15:40:42 +0000

    Updated the UIs of the forms to (more or less) match the design

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char * argv[]) {
     QApplication a(argc, argv);
 
     for (int i = 0; i < argc; ++i) {
-        if ((strcmp(argv[i], "-t") == 1) || (strcmp(argv[i], "--test") == 1) || true) {
+        if ((strcmp(argv[i], "-t") == 1) || (strcmp(argv[i], "--test") == 1)) {
             ::testing::InitGoogleTest(&argc, argv);
             return RUN_ALL_TESTS();;
         }
